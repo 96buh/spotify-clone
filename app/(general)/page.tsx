7f5abcf8bac6f99cc51e1926478f390613baac75
Homer <j29823678@gmail.com>
2024-05-18 19:25:03 +0800

    add loading card skeletons

--- app/(general)/page.tsx
+++ app/(general)/page.tsx
@@ -2,7 +2,7 @@ import PopularAlbums from "@/components/PopularAlbums";
 import PopularArtists from "@/components/PopularArtists";
 export default async function HomePage() {
     return (
-        <div className="px-2 py-2 bg-primary flex-grow overflow-hidden hover:overflow-y-auto">
+        <div className=" py-2 bg-primary flex-grow overflow-hidden hover:overflow-y-auto">
             <PopularArtists />
 
             <PopularAlbums />
